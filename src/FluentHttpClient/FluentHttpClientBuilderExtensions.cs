95d85be5b5249ef911f633b974397be5f14d8b1a
Ahmad <achehre@yahoo.com>
2018-10-09 14:23:26 +0330

    As methods moved to builder extension

---
+++ src/FluentHttpClient/FluentHttpClientBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentHttpClient
+{
+    public static class FluentHttpClientBuilderExtensions
+    {
+        public static FluentHttpClientRequest.FluentHttpClientRequestBuilder AsPost(this FluentHttpClientRequest.FluentHttpClientRequestBuilder builder)
+        {
+            builder.WithMethod(HttpMethod.Post);
+            return builder;
+        }
+
+        public static FluentHttpClientRequest.FluentHttpClientRequestBuilder AsGet(this FluentHttpClientRequest.FluentHttpClientRequestBuilder builder)
+        {
+            builder.WithMethod(HttpMethod.Get);
+            return builder;
+        }
+
+        public static FluentHttpClientRequest.FluentHttpClientRequestBuilder AsPut(this FluentHttpClientRequest.FluentHttpClientRequestBuilder builder)
+        {
+            builder.WithMethod(HttpMethod.Put);
+            return builder;
+        }
+
+        public static FluentHttpClientRequest.FluentHttpClientRequestBuilder AsDelete(this FluentHttpClientRequest.FluentHttpClientRequestBuilder builder)
+        {
+            builder.WithMethod(HttpMethod.Delete);
+            return builder;
+        }
+    }
+}
--- src/FluentHttpClient/FluentHttpClientExtensions.cs
+++ src/FluentHttpClient/FluentHttpClientExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Formatting;
+﻿using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 
 namespace FluentHttpClient
@@ -11,6 +12,18 @@ namespace FluentHttpClient
             var mediaType = JsonMediaTypeFormatter.DefaultMediaType.MediaType;
             var request = FluentHttpClientRequest.CreateNewRequest().AsPost().WithUri(uri)
                 .WithBody(body, jsonMediaTypeFormatter, mediaType).Build();
+            var response = await fluentHttpClient.SendAsync<T>(request);
+            return response.Content;
+        }
+
+
+       
+
+
+
+        public static async Task<T> GetAsJson<T>(this FluentHttpClient fluentHttpClient, string uri)
+        {
+            var request = FluentHttpClientRequest.CreateNewRequest().AsGet().WithUri(uri).Build();
             var response = await fluentHttpClient.SendAsync<T>(request);
             return response.Content;
         }
